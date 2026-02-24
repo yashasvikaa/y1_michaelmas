671d4d7b8d8ac1a1f33534b4a9b56a42ea5d1fde
Yashasvika Ahuja <yashasvikaahuja@gmail.com>
2026-02-24 13:47:18 +0000

    Add files via upload

---
+++ fp/sheet03.hs
@@ -0,0 +1,12 @@
+square :: Integer -> Integer
+square x = x * x 
+
+mod :: Integer -> Integer -> Integer
+mod x y = x - (x `div` y) * y
+
+sqrt :: Integer -> Integer 
+sqrt x = 
+
+largest :: Integer -> Integer
+largest x  = (sqrt x) * (sqrt x)
+
